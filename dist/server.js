6efd77ccf1942c3d1e2cb53b409e5e0395b4eee2
muhammad-jiku <muhammadjiku364@gmail.com>
2022-11-11 00:16:47 +0600

    format established

--- dist/server.js
+++ dist/server.js
@@ -1,3 +1,24 @@
 "use strict";
-console.log("HELLO! ki obostha sobar??");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// external imports
+const express_1 = __importDefault(require("express"));
+// initializing app config
+const init = () => {
+    let app = (0, express_1.default)();
+    app.use("/", (req, res) => {
+        res.send({ message: "Welcome here!" });
+    });
+    return app;
+};
+// declaring port
+const port = (process.env.PORT || 4000);
+// calling init function
+const app = init();
+// listening to the port
+app.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`);
+});
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,24 @@
-console.log("HELLO! ki obostha sobar??");
+// external imports
+import express, { Application } from "express";
+
+// initializing app config
+const init = (): Application => {
+  let app = express();
+
+  app.use("/", (req, res) => {
+    res.send({ message: "Welcome here!" });
+  });
+
+  return app;
+};
+
+// declaring port
+const port = (process.env.PORT || 4000) as Number;
+
+// calling init function
+const app = init();
+
+// listening to the port
+app.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`);
+});
