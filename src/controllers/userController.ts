d18855973ba3445cf3f7abceb0f4feb9fb50e564
muhammad-jiku <muhammadjiku364@gmail.com>
2022-11-12 19:52:08 +0600

    controllers addded

---
+++ src/controllers/userController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { omit } from "lodash";
+
+// internal imports
+import { createUser } from "../services/userServices";
+import log from "../utilities/logger";
+
+export async function createUserHandler(req: Request, res: Response) {
+  try {
+    const user = await createUser(req.body);
+    log.info(user);
+  } catch (e: any) {
+    log.error(e);
+    return res.status(500).send(e.message);
+  }
+}
